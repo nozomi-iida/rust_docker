9961f59af2bff0c532fd35de5e803359d811e0f9
nozomi-iida <iida19990106@gmail.com>
2021-06-26 15:49:35 +0900

    feat: set up rust postgres with docker

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,9 @@ FROM rust:1.53.0
 
 WORKDIR /app
 
+RUN apt-get update -qq && \
+    rm -rf /var/lib/apt/lists/* && \
+    cargo install cargo-watch && \
+    cargo install diesel_cli --no-default-features --features postgres
+
 COPY . .
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use dotenv::dotenv;
+
+mod employees;
+
+async fn welcome(request: HttpRequest) -> impl Responder {
+    let name = request.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    HttpServer::new(|| {
+        App::new()
+          .route("/", web::get().to(welcome))
+          .route("/{name}", web::get().to(welcome))
+          .configure(employees::init_routes)
+    })
+      .bind("0.0.0.0:8000")?
+      .run()
+      .await
 }
